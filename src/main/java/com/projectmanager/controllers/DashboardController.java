902e320d9391b1e629a7c9df2372c781d3358902
Hayes Roach <hhroach@live.com>
2019-10-23 15:48:44 -0400

    Added delete project functionality

--- src/main/java/com/projectmanager/controllers/DashboardController.java
+++ src/main/java/com/projectmanager/controllers/DashboardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -52,6 +53,16 @@ public class DashboardController {
         model.addAttribute("project", project);
 
         return "view-license";
+    }
+
+    @PostMapping("/delete-project")
+    public String deleteProject(@ModelAttribute Project project) {
+
+        String id = project.getId();
+
+        projectService.deleteProject(id);
+
+        return "redirect:/";
     }
 
 
