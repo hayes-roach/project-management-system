2b5bd7b5699ee837094ced95fd92186b4fd3cf83
Hayes Roach <roachbaseball@gmail.com>
2019-11-06 17:32:57 -0500

    Added skeleton for monitoring and tracking page and also added sample pie graph that needs to be replaced by actual data

--- src/main/java/com/projectmanager/controllers/DashboardController.java
+++ src/main/java/com/projectmanager/controllers/DashboardController.java
@@ -1,5 +1,6 @@
 package com.projectmanager.controllers;
 
+import com.google.gson.Gson;
 import com.projectmanager.data.dao.FunctionalReqRepository;
 import com.projectmanager.data.dao.ProjectRepository;
 import com.projectmanager.data.dao.RiskRepository;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DashboardController {
@@ -79,7 +83,7 @@ public class DashboardController {
     }
 
     @GetMapping("/view-project")
-    public String viewProject(Model model, @RequestParam String id) {
+    public String viewProject(HttpServletRequest request, Model model, @RequestParam String id) {
 
         List<Project> projects = projectRepository.findAll();
 
@@ -104,6 +108,31 @@ public class DashboardController {
         List<NonFunctionalRequirement> nonFunctionalRequirements = requirementService.getAllNonFunctionalRequirementsByProjectId(id);
 
         model.addAttribute("nonFunctionalRequirements", nonFunctionalRequirements);
+
+        HttpSession session = request.getSession();
+
+        Gson gsonObj = new Gson();
+        Map<Object,Object> map = null;
+        List<Map<Object,Object>> list = new ArrayList<Map<Object,Object>>();
+
+        map = new HashMap<Object,Object>(); map.put("x", 10); map.put("y", 31); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 20); map.put("y", 65); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 30); map.put("y", 40); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 40); map.put("y", 84); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 50); map.put("y", 68); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 60); map.put("y", 64); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 70); map.put("y", 38); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 80); map.put("y", 71); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 90); map.put("y", 54); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 100); map.put("y", 60); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 110); map.put("y", 21); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 120); map.put("y", 49); list.add(map);
+        map = new HashMap<Object,Object>(); map.put("x", 130); map.put("y", 41); list.add(map);
+
+        String dataPoints = gsonObj.toJson(list);
+
+        session.setAttribute("dataPoints", dataPoints);
+        System.out.println(dataPoints);
 
         return "view-project";
     }
