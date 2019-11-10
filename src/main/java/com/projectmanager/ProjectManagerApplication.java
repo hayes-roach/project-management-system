3f080e0da30c9484c396ea6e151f07df985cd176
Hayes Roach <roachbaseball@gmail.com>
2019-11-09 20:16:06 -0500

    Test

--- src/main/java/com/projectmanager/ProjectManagerApplication.java
+++ src/main/java/com/projectmanager/ProjectManagerApplication.java
@@ -2,12 +2,21 @@ package com.projectmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
-public class ProjectManagerApplication {
+@SpringBootApplication()
+public class ProjectManagerApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagerApplication.class, args);
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/").setViewName("dashboard");
 	}
 
 }
